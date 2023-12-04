495bdcd45d62c9aa092ec0869a0eb7d3d9eb90a7
Lon Beshiri <lonbeshiri@gmail.com>
2023-12-04 14:41:39 -0500

    feat: build

--- dist/types/src/components/ArtUpload.d.ts
+++
@@ -1,7 +0,0 @@
-import { UploadRelease } from '../types';
-type ArtUploadProps = {
-    release: UploadRelease;
-    setReleaseProperty: Function;
-};
-export default function ArtUpload({ release, setReleaseProperty, }: ArtUploadProps): import("react/jsx-runtime").JSX.Element;
-export {};
